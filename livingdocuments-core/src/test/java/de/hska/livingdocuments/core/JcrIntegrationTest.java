3f7ea9417ea89b0cd14b53336cf45b6ace64c414
Dennis Soehnen <info@soehnen.com>
2014-05-06 21:06:59 +0200

    Refactor JCR integration test

--- livingdocuments-core/src/test/java/de/hska/livingdocuments/core/JcrIntegrationTest.java
+++ livingdocuments-core/src/test/java/de/hska/livingdocuments/core/JcrIntegrationTest.java
@@ -22,80 +22,111 @@
 
 package de.hska.livingdocuments.core;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.jackrabbit.JcrConstants;
+import org.apache.jackrabbit.api.JackrabbitSession;
+import org.apache.jackrabbit.api.security.user.AuthorizableExistsException;
+import org.apache.jackrabbit.api.security.user.User;
+import org.apache.jackrabbit.api.security.user.UserManager;
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.jcr.*;
+import java.io.InputStream;
+import java.util.Calendar;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class JcrIntegrationTest extends AbstractIntegrationTest {
+    static final Logger LOGGER = Logger.getLogger(JcrIntegrationTest.class);
 
-    Credentials credentials = new SimpleCredentials("admin", "admin".toCharArray());
-
+    static final String TEST_USER = "testuser";
+    Credentials userCredentials = new SimpleCredentials(TEST_USER, TEST_USER.toCharArray());
+    static final String TEST_FOLDER = "testfolder";
+    static final String TEST_PDF = "test.pdf";
+    Credentials adminCredentials = new SimpleCredentials("admin", "admin".toCharArray());
     @Autowired
     Repository repository;
 
-    @Test
-    public void dummyTest() throws RepositoryException {
-        Session session = repository.login(credentials);
-
+    @Before
+    public void setUp() throws Exception {
+        JackrabbitSession session = (JackrabbitSession) repository.login(adminCredentials);
+        // Create test user
+        UserManager userManager = session.getUserManager();
+        try {
+            userManager.createUser(TEST_USER, TEST_USER);
+        } catch (AuthorizableExistsException e) {
+            LOGGER.info("Test user already exists.");
+        }
+        // Create test folder
         Node root = session.getRootNode();
-        String nodeName = "testNode";
-        Node node = root.getNode(nodeName);
-        if (!root.hasNode(nodeName)) {
-            session.save();
+        try {
+            root.addNode(TEST_FOLDER, JcrConstants.NT_FOLDER);
+        } catch (ItemExistsException e) {
+            LOGGER.info("Test folder already exists.");
         }
 
-        assertNotNull("Node is null.", node);
+        session.save();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        JackrabbitSession session = (JackrabbitSession) repository.login(adminCredentials);
+        // Remove test folder
+        Node root = session.getRootNode();
+        Node folderNode = root.getNode(TEST_FOLDER);
+        folderNode.remove();
+        // Remove test user
+        UserManager userManager = session.getUserManager();
+        User user = (User) userManager.getAuthorizable(TEST_USER);
+        user.remove();
+
+        session.save();
     }
 
     @Test
-    public void testAddFileIfDoesNotExist() {
-        /*@SuppressWarnings("unused")
-        Node node = (Node) template.execute(new JcrCallback() {
-            @SuppressWarnings("unchecked")
-            public Object doInJcr(Session session) throws RepositoryException, IOException {
-                Node resultNode = null;
-                Node root = session.getRootNode();
-                LOGGER.info("starting from root node.  node={}", root);
+    public void thatFileNodeIsCreated() throws RepositoryException {
+        Session session = repository.login(adminCredentials);
 
-                // should have been created in previous test
-                Node folderNode = root.getNode(nodeName);
-                String fileName = "test.pdf";
+        Node fileNode;
+        Node resultNode;
+        Node root = session.getRootNode();
 
-                if (folderNode.hasNode(fileName)) {
-                    LOGGER.debug("File already exists.  file={}", fileName);
-                } else {
-                    InputStream in = JcrIntegrationTest.class.getResourceAsStream("/" + fileName);
-                    ValueFactory factory = session.getValueFactory();
-                    Binary binary = factory.createBinary(in);
-                    Node fileNode = folderNode.addNode(fileName, JcrConstants.NT_FILE);
+        Node folderNode = root.getNode(TEST_FOLDER);
 
-                    // create the mandatory child node - jcr:content
-                    resultNode = fileNode.addNode(JcrConstants.JCR_CONTENT, JcrConstants.NT_RESOURCE);
-                    resultNode.setProperty(JcrConstants.JCR_MIMETYPE, "application/pdf");
-                    //resultNode.setProperty(JcrConstants.JCR_ENCODING, "UTF-8");
-                    resultNode.setProperty(JcrConstants.JCR_DATA, binary);
-                    Calendar lastModified = Calendar.getInstance();
-                    lastModified.setTimeInMillis(System.currentTimeMillis());
-                    resultNode.setProperty(JcrConstants.JCR_LASTMODIFIED, lastModified);
+        if (folderNode.hasNode(TEST_PDF)) {
+            LOGGER.debug("File already exists. file=" + TEST_PDF);
+        } else {
+            InputStream in = JcrIntegrationTest.class.getResourceAsStream("/" + TEST_PDF);
+            ValueFactory factory = session.getValueFactory();
+            Binary binary = factory.createBinary(in);
+            fileNode = folderNode.addNode(TEST_PDF, JcrConstants.NT_FILE);
 
-                    session.save();
-                    IOUtils.closeQuietly(in);
-                    LOGGER.debug("Created '{}' file in folder.", fileName);
-                }
+            // create the mandatory child node - jcr:content
+            resultNode = fileNode.addNode(JcrConstants.JCR_CONTENT, JcrConstants.NT_RESOURCE);
+            resultNode.setProperty(JcrConstants.JCR_MIMETYPE, "application/pdf");
+            resultNode.setProperty(JcrConstants.JCR_DATA, binary);
+            Calendar lastModified = Calendar.getInstance();
+            lastModified.setTimeInMillis(System.currentTimeMillis());
+            resultNode.setProperty(JcrConstants.JCR_LASTMODIFIED, lastModified);
 
-                assertTrue("File node, '" + fileName + "', doesn't exist.", folderNode.hasNode(fileName));
-                assertTrue("File content node, '" + fileName + "', doesn't exist.",
-                        folderNode.getNode(fileName).hasNode(JcrConstants.JCR_CONTENT));
+            session.save();
+            IOUtils.closeQuietly(in);
+            LOGGER.debug("Created '" + TEST_PDF + "' in " + TEST_FOLDER);
+        }
 
-                Node contentNode = folderNode.getNode(fileName).getNode(JcrConstants.JCR_CONTENT);
-                Property dataProperty = contentNode.getProperty(JcrConstants.JCR_DATA);
-                assertNotNull(dataProperty);
+        assertTrue("File node, '" + TEST_PDF + "', doesn't exist.", folderNode.hasNode(TEST_PDF));
+        assertTrue("File content node, '" + TEST_PDF + "', doesn't exist.",
+                folderNode.getNode(TEST_PDF).hasNode(JcrConstants.JCR_CONTENT));
 
-                return resultNode;
-            }
-        });*/
+        Node contentNode = folderNode.getNode(TEST_PDF).getNode(JcrConstants.JCR_CONTENT);
+        Property dataProperty = contentNode.getProperty(JcrConstants.JCR_DATA);
+        assertNotNull(dataProperty);
+
+        //JcrUtils.readFile(fileNode);
     }
 }
