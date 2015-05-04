40ff1de7319cfc9e8f5fb5dd3a5996e592717eff
Gregory Villicana <gregory.villicana@gmail.com>
2015-05-04 18:21:50 +0200

    Rejected commit: Default

---
+++ ld-ldocsToSSS/src/main/java/de/hska/ld/ldToSSS/persistence/domain/LDocToSSSInfo.java
@@ -0,0 +1,79 @@
+/**
+ * Code contributed to the Learning Layers project
+ * http://www.learning-layers.eu
+ * Development is partly funded by the FP7 Programme of the European
+ * Commission under Grant Agreement FP7-ICT-318209.
+ * Copyright (c) 2014, Karlsruhe University of Applied Sciences.
+ * For a list of contributors see the AUTHORS file at the top-level directory
+ * of this distribution.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.hska.ld.ldToSSS.persistence.domain;
+
+import de.hska.ld.content.persistence.domain.Document;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ld_document_etherpad_info")
+public class DocumentEtherpadInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "group_pad_id")
+    private String groupPadId;
+
+    @Column(name = "read_only_id")
+    private String readOnlyId;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "document_id")
+    private Document document;
+
+    public String getReadOnlyId() {
+        return readOnlyId;
+    }
+
+    public void setReadOnlyId(String readOnlyId) {
+        this.readOnlyId = readOnlyId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGroupPadId() {
+        return groupPadId;
+    }
+
+    public void setGroupPadId(String groupPadId) {
+        this.groupPadId = groupPadId;
+    }
+
+    public Document getDocument() {
+        return document;
+    }
+
+    public void setDocument(Document document) {
+        this.document = document;
+    }
+}
