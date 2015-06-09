a779ed1556c6426a9a91c27fae15b032595ede59
Gregory Villicana <gregory.villicana@gmail.com>
2015-06-09 22:27:39 +0200

    Module should work outside and within the hochschule, just have to add the proper username and password in RecommConfig and change the flag of RequiresProxy to true ;)

---
+++ ld-content/src/main/java/de/hska/ld/content/persistence/domain/Category.java
@@ -0,0 +1,76 @@
+/**
+ * Code contributed to the Learning Layers project
+ * http://www.learning-layers.eu
+ * Development is partly funded by the FP7 Programme of the European
+ * Commission under Grant Agreement FP7-ICT-318209.
+ * Copyright (c) 2014, Karlsruhe University of Applied Sciences.
+ * For a list of contributors see the AUTHORS file at the top-level directory
+ * of this distribution.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.hska.ld.content.persistence.domain;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ld_category")
+public class Category extends Content {
+
+    @NotBlank
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category)) return false;
+
+        Category category = (Category) o;
+
+        return !(description != null ? !description.equals(category.description) : category.description != null) &&
+                !(name != null ? !name.equals(category.name) : category.name != null);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+}
