30304558b7349af2b2202b590987c12c9cbe9537
Martin Bachl <martin@bachl.pro>
2015-07-24 09:26:45 +0200

    - Added settings path environment variable.

--- docker/inner-run1.sh
+++ docker/inner-run1.sh
@@ -2,6 +2,7 @@
 sudo echo $HTTP_USER
 sudo echo $HTTP_PASSWORD
 sudo echo $HTTP_BASE_URL
-sudo echo "wget --http-user=$HTTP_USER --http-password=$HTTP_PASSWORD $HTTP_URL/keystore.jks"
+sudo echo $SETTINGS_PATH
+sudo echo "wget --http-user=$HTTP_USER --http-password=$HTTP_PASSWORD $HTTP_URL/$SETTINGS_PATH/https/keystore.jks"
 cd /opt/ldserver
-sudo wget -N --http-user=$HTTP_USER --http-password=$HTTP_PASSWORD $HTTP_BASE_URL/keystore.jks
+sudo wget -N --http-user=$HTTP_USER --http-password=$HTTP_PASSWORD $HTTP_BASE_URL/$SETTINGS_PATH/https/keystore.jks
