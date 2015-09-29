580a4515126ac8ac49773df75ddd1bf4aa18fe75
Martin Bachl <martin@bachl.pro>
2015-09-29 11:52:40 +0200

    - Added getters and setters for fileDto.

--- ld-oidc/src/main/java/de/hska/ld/oidc/dto/SSSFileDto.java
+++ ld-oidc/src/main/java/de/hska/ld/oidc/dto/SSSFileDto.java
@@ -4,4 +4,28 @@ public class SSSFileDto extends SSSEntityDto {
     String fileExt;
     String mimeType;
     String downloadLink;
+
+    public String getFileExt() {
+        return fileExt;
+    }
+
+    public void setFileExt(String fileExt) {
+        this.fileExt = fileExt;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public void setMimeType(String mimeType) {
+        this.mimeType = mimeType;
+    }
+
+    public String getDownloadLink() {
+        return downloadLink;
+    }
+
+    public void setDownloadLink(String downloadLink) {
+        this.downloadLink = downloadLink;
+    }
 }
