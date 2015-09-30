8dac30d0f2601c4cc9d9a4d3538cea429a876bc4
Martin Bachl <martin@bachl.pro>
2015-09-30 09:54:04 +0200

    - Added tagsDtos to qa entries.

--- ld-oidc/src/main/java/de/hska/ld/oidc/dto/SSSQAEntryDto.java
+++ ld-oidc/src/main/java/de/hska/ld/oidc/dto/SSSQAEntryDto.java
@@ -7,6 +7,7 @@ public class SSSQAEntryDto extends SSSEntityDto {
     String description;
     String content;
     List<SSSEntityDto> attachedEntities;
+    List<SSSTagDto> tags;
 
     public String getDescription() {
         return description;
@@ -30,5 +31,13 @@ public class SSSQAEntryDto extends SSSEntityDto {
 
     public void setAttachedEntities(List<SSSEntityDto> attachedEntities) {
         this.attachedEntities = attachedEntities;
+    }
+
+    public List<SSSTagDto> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<SSSTagDto> tags) {
+        this.tags = tags;
     }
 }
