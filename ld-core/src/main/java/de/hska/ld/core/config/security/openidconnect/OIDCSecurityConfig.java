5035f873557f3c733ca5ce5df15cfd382ec57c91
Martin Bachl <martin@bachl.pro>
2015-11-25 06:51:25 +0100

    - removed unnecessary code.

--- ld-core/src/main/java/de/hska/ld/core/config/security/openidconnect/OIDCSecurityConfig.java
+++ ld-core/src/main/java/de/hska/ld/core/config/security/openidconnect/OIDCSecurityConfig.java
@@ -1,3 +1,25 @@
+/*
+ *  Code contributed to the Learning Layers project
+ *  http://www.learning-layers.eu
+ *  Development is partly funded by the FP7 Programme of the European
+ *  Commission under Grant Agreement FP7-ICT-318209.
+ *  Copyright (c) 2015, Karlsruhe University of Applied Sciences.
+ *  For a list of contributors see the AUTHORS file at the top-level directory
+ *  of this distribution.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package de.hska.ld.core.config.security.openidconnect;
 
 import de.hska.ld.core.config.security.AjaxAuthenticationFailureHandler;
@@ -167,18 +189,7 @@ public class OIDCSecurityConfig extends WebSecurityConfigurerAdapter {
                 SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
                 try {
                     Date date = sdf.parse(oidcUpdatedTime);
-                    if (currentUserInDb.getLastupdatedAt().getTime() > date.getTime()) {
-                        User userWithGivenPreferredUserName = userService.findByUsername(oidcUserInfo.getPreferredUsername());
-                        int i = 0;
-                        if (userWithGivenPreferredUserName != null) {
-                            while (userWithGivenPreferredUserName != null) {
-                                String prefferedUsername = oidcUserInfo.getPreferredUsername() + "#" + i;
-                                userWithGivenPreferredUserName = userService.findByUsername(prefferedUsername);
-                            }
-                        } else {
-                            currentUserInDb.setUsername(oidcUserInfo.getPreferredUsername());
-                        }
-
+                    if (currentUserInDb.getEmail() == null || currentUserInDb.getLastupdatedAt().getTime() > date.getTime()) {
                         currentUserInDb.setFullName(oidcUserInfo.getName());
                         currentUserInDb.setEmail(oidcUserInfo.getEmail());
                         currentUserInDb = userService.save(currentUserInDb);
@@ -200,7 +211,7 @@ public class OIDCSecurityConfig extends WebSecurityConfigurerAdapter {
                         userWithGivenPreferredUserName = userService.findByUsername(prefferedUsername);
                     }
                 } else {
-                    user.setUsername(oidcUserInfo.getPreferredUsername());
+                    user.setUsername(oidcUserInfo.getPreferredUsername() + "#" + i);
                 }
 
                 user.setFullName(oidcUserInfo.getName());
